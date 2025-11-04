f1a82cdb99531428d81b3c186fc8e56aed0eff37
Miguel-Vieira006 <miguelvieiraxv@gmail.com>
2025-11-04 18:37:52 -0300

    feat: Adicionando livros

--- Livros_sqlites.py
+++ Livros_sqlites.py
@@ -12,3 +12,14 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS livros (id INTEGER PRIMARY KEY AUTO
                genero TEXT, 
                disponivel BOOLEAN)""")
 banco.commit()
+
+#inserir 5 livros na tabela
+cursor.execute("INSERT INTO livros (titulo, autor, ano, genero) VALUES ('Abuela', 'Daniel Pinheiro', 2025, 'Ficção')")
+cursor.execute("INSERT INTO livros (titulo, autor, ano, genero) VALUES ('O Assassino da Faca de Pão', 'Ana Maria Braga', 1870, 'Suspense')")
+cursor.execute("INSERT INTO livros (titulo, autor, ano, genero) VALUES  ('O Mago Holandes', 'Michael Jackson', 2035, 'Fantasia')")
+cursor.execute("INSERT INTO livros (titulo, autor, ano, genero) VALUES  ('Queijo Quente', 'Mikey Mouse', 1920, 'Terror')")
+cursor.execute("INSERT INTO livros (titulo, autor, ano, genero) VALUES  ('Salsicha Gigante', 'Cachorro da Esquina', 2000,'Comédia')")
+banco.commit()
+
+
+
