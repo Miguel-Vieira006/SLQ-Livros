0d5163eb2d1ed42cfb903810090852ba285a76e7
Miguel-Vieira006 <miguelvieiraxv@gmail.com>
2025-11-04 17:07:51 -0300

    feat: criar tabela livros

--- Livros_sqlites.py
+++ Livros_sqlites.py
@@ -1,1 +1,14 @@
 import sqlite3
+
+banco = sqlite3.connect('livros.db')
+
+cursor = banco.cursor()
+
+#criar uma tabela chamada livros com id titulo autoe ano genero e disponivel
+cursor.execute("""CREATE TABLE IF NOT EXISTS livros (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+               titulo TEXT, 
+               autor TEXT, 
+               ano INTEGER, 
+               genero TEXT, 
+               disponivel BOOLEAN)""")
+banco.commit()
